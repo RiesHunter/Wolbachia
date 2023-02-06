bbab2273801d96ed83b6ff8d13fc7b82aebbf737
RiesHunter <Hries@wisc.edu>
2023-02-05 19:08:02 -0600

    Starting barcode code code code

---
+++ code/2-count_barcodes.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+## Start time
+SECONDS=0
+echo ""; echo "-------------------- Starting barcode analyses... --------------------"
+
+
+### ---------------------------------------------------------------------------------------------------- ###
+## Parameters
+workingdir="/Users/rieshunter/Documents/bioinformatics/Wolbachia/data/reads/data/run"
+reference="/Users/rieshunter/Documents/bioinformatics/Wolbachia/data/refseqs/PRVABC59.fasta"
+cd $workingdir
+pwd
+
+exit 1
+
+
+
+## Set the sample name variable
+echo "What is the name of the sample?"
+read sample
+
+## Change directory to the output directory for rep 1
+cd PATH/TO/${sample}_rep1_v3.1
+
+## Align processed paired-end reads to reference with default settings
+bwa mem -t 4 PATH/TO/REFERENCE.fasta ./normalized_fastq_bam/norm_${sample}_1_r1.fastq ./normalized_fastq_bam/norm_${sample}_1_r2.fastq > relaxed_norm_${sample}_rep1.bam
+
+## sort and index alignment
+samtools sort ./relaxed_norm_${sample}_rep1.bam > sorted_relaxed_norm_${sample}_rep1.bam
+samtools index ./sorted_relaxed_norm_${sample}_rep1.bam
+
+## extract aligned reads overlapping the barcode region
+samtools view -h ./sorted_relaxed_norm_${sample}_rep1.bam "KU501215_1:4007-4030" > BCregion_sorted_relaxed_norm_${sample}_rep1.bam
+
+## convert aligned reads to fastq
+bamToFastq -i ./BCregion_sorted_relaxed_norm_${sample}_rep1.bam -fq BCregion_sorted_relaxed_${sample}_rep1.fastq
+
+## convert fastq to fasta
+seqtk seq -a ./BCregion_sorted_relaxed_${sample}_rep1.fastq > BCregion_sorted_relaxed_${sample}_rep1.fasta
+
+## extract just the barcode sequence from all the reads
+sed -n '/^>/p; /CT.GC.GC.CT.AC.CC.CT.GC./p' ./BCregion_sorted_relaxed_${sample}_rep1.fasta > sed_BCregion_sorted_relaxed_${sample}_rep1.fasta
+sed -e '$!N;/^>.*\n>/D' -e 'P;D' ./sed_BCregion_sorted_relaxed_${sample}_rep1.fasta > sedfilter_BCregion_sorted_relaxed_${sample}_rep1.fasta
+sed 's/^.*\(CT.GC.GC.CT.AC.CC.CT.GC.\).*$/\1/' ./sedfilter_BCregion_sorted_relaxed_${sample}_rep1.fasta > BConly_${sample}_rep1.fasta
+
+## Count kmers
+PATH/TO/kmercountexact.sh in=./BConly_${sample}_rep1.fasta out=kmer_counts_${sample}_rep1.fasta k=24 khist=kmer_freq_hist_${sample}_rep1.tsv fastadump=t rcomp=f ow=t
+
+## Generate tsv for barcode counts
+seqkit fx2tab ./kmer_counts_${sample}_rep1.fasta -H > counts_BConly_${sample}_rep1.tsv
+
+## Clean up
+mkdir ./barcode_analyses
+mv *relaxed*.fastq ./barcode_analyses
+mv *.fasta ./barcode_analyses
+mv *relaxed*.bam ./barcode_analyses
+mv *relaxed*.bam.bai ./barcode_analyses
+mv *.tsv ./barcode_analyses
+
+
+## Change directory to the output directory for rep 2
+cd PATH/TO/${sample}_rep2_v3.1
+
+## Align processed paired-end reads to reference with default settings
+bwa mem -t 4 PATH/TO/REFERENCE.fasta ./normalized_fastq_bam/norm_${sample}_2_r1.fastq ./normalized_fastq_bam/norm_${sample}_2_r2.fastq > relaxed_norm_${sample}_rep2.bam
+
+## sort and index alignment
+samtools sort ./relaxed_norm_${sample}_rep2.bam > sorted_relaxed_norm_${sample}_rep2.bam
+samtools index ./sorted_relaxed_norm_${sample}_rep2.bam
+
+## extract aligned reads overlapping the barcode region
+samtools view -h ./sorted_relaxed_norm_${sample}_rep2.bam "KU501215_1:4007-4030" > BCregion_sorted_relaxed_norm_${sample}_rep2.bam
+
+## convert aligned reads to fastq
+bamToFastq -i ./BCregion_sorted_relaxed_norm_${sample}_rep2.bam -fq BCregion_sorted_relaxed_${sample}_rep2.fastq
+
+## convert fastq to fasta
+seqtk seq -a ./BCregion_sorted_relaxed_${sample}_rep2.fastq > BCregion_sorted_relaxed_${sample}_rep2.fasta
+
+## extract just the barcode sequence from all the reads
+sed -n '/^>/p; /CT.GC.GC.CT.AC.CC.CT.GC./p' ./BCregion_sorted_relaxed_${sample}_rep2.fasta > sed_BCregion_sorted_relaxed_${sample}_rep2.fasta
+sed -e '$!N;/^>.*\n>/D' -e 'P;D' ./sed_BCregion_sorted_relaxed_${sample}_rep2.fasta > sedfilter_BCregion_sorted_relaxed_${sample}_rep2.fasta
+sed 's/^.*\(CT.GC.GC.CT.AC.CC.CT.GC.\).*$/\1/' ./sedfilter_BCregion_sorted_relaxed_${sample}_rep2.fasta > BConly_${sample}_rep2.fasta
+
+## Count kmers
+PATH/TO/kmercountexact.sh in=./BConly_${sample}_rep2.fasta out=kmer_counts_${sample}_rep2.fasta k=24 khist=kmer_freq_hist_${sample}_rep2.tsv fastadump=t rcomp=f ow=t
+
+## Generate tsv for barcode counts
+seqkit fx2tab ./kmer_counts_${sample}_rep2.fasta -H > counts_BConly_${sample}_rep2.tsv
+
+## Clean up
+mkdir ./barcode_analyses
+mv *relaxed*.fastq ./barcode_analyses
+mv *.fasta ./barcode_analyses
+mv *relaxed*.bam ./barcode_analyses
+mv *relaxed*.bam.bai ./barcode_analyses
+mv *.tsv ./barcode_analyses
+
+exit 1
--- repro.md
+++ repro.md
@@ -1,15 +1,26 @@
 ## Project overview
-- Mosquitoes fed on blood or ZIKV-infected mice
-- Some mosquitoes were "infected" with Wmel (*Wolbachia sp.*); others were given Tetracycline ("Tet")
-- We see reduced ZIKV dissemination (movement from midgut to legs to saliva) in the Wmel group
-- We are interested in how the ZIKV population evolves during Wolbachia infection
+ - Mosquitoes fed on blood or ZIKV-infected mice
+ - Some mosquitoes were "infected" with Wmel (*Wolbachia sp.*); others were given Tetracycline ("Tet")
+ - We see reduced ZIKV dissemination (movement from midgut to legs to saliva) in the Wmel group
+ - We are interested in how the ZIKV population evolves during Wolbachia infection
 
 ## Goal
-- Calculate Pi, PiN/PiS, mutation frequency, Gini-Simpson, and Shannon Entropy for all samples
-- Compare metrics between Tet and Wmel over time
-- Investigate bottleneck sizes through barcode analyses
+ - Calculate Pi, PiN/PiS, mutation frequency, Gini-Simpson, and Shannon Entropy for all samples
+ - Compare metrics between Tet and Wmel over time
+ - Investigate bottleneck sizes through barcode analyses
 
 # 230201
 ## Objectives:
-- Calculate barcode frequencies
-- Compare Tet to Wmel over time
+ - Calculate barcode frequencies
+ - Compare Tet to Wmel over time
+
+### Process Illumina WGS fastq.gz files
+Adapted file "ZIKV_OverlappingAmplicon_WGS_PE150_v3.1_LOOP2.command" as "/code/1-process.sh"
+ - removed multi-lane processing
+Adapted file "ZIKV_WGS_pipeline_Diversity_dinuc_v1_1.R" as "/code/RStudio/1-process.R"
+ - removed multi-lane processing
+ - changed data output to various CSVs
+
+### Extract and count barcodes
+Adapted file "ZIKV_WGS_Barcode_analyses.command" as "/code/2-count_barcodes.sh" 
+ - 
